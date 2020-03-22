1afebde5c23ea783fd6376a4c294646473f25e9b
kaklakariada <kaklakariada@users.noreply.github.com>
2020-03-22 12:01:16 +0100

    Add static content component

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { InfrastructureStack, InfrastructureStackProps } from '../lib/infrastructure-stack';
+import { CONFIG } from "../infrastructure-config";
+import { InfrastructureConfig } from "../lib/infrastructure-config-interface";
+
+const config: InfrastructureConfig = CONFIG;
+
+const props: InfrastructureStackProps = {
+    env: { region: config.region },
+    description: `S3 Media Player ${config.domain}`,
+    tags: { stack: config.stackName },
+    domain: config.domain,
+    hostedZoneName: config.hostedZoneName,
+    sslCertificateArn: config.sslCertificateArn
+};
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
+new InfrastructureStack(app, 'InfrastructureStack', props);
---
+++ infrastructure/lib/infrastructure-config-interface.ts
@@ -0,0 +1,8 @@
+export interface InfrastructureConfig {
+    region: string;
+    domain: string;
+    hostedZoneName: string;
+    sslCertificateArn: string;
+    stackName: string;
+    contactEmailAddress: string;
+}
