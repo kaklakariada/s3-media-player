5c0e93b4d0646ed2b80e1360624209ba92f15676
kaklakariada <kaklakariada@users.noreply.github.com>
2020-04-05 11:59:11 +0200

    Deploy frontend to cloudfront

---
+++ frontend/deploy/deploy.js
@@ -0,0 +1,17 @@
+const { spawnSync } = require('child_process');
+const { CONFIG } = require('./deploy-config');
+
+function execAws(args) {
+    const environment = Object.create(process.env);
+    environment.AWS_PAGER = '';
+    spawnSync('aws', args, { stdio: 'inherit', env: environment });
+}
+
+const bucket = CONFIG.staticWebsiteBucket;
+const cloudfrontDistribution = CONFIG.cloudfrontDistributionId;
+
+console.log(`Uploading static content to bucket ${bucket}...`);
+execAws(['s3', 'sync', 'build', `s3://${bucket}/`]);
+console.log(`Invalidating cache for Cloudfront distribution ${cloudfrontDistribution}...`);
+execAws(['cloudfront', 'create-invalidation', '--distribution-id', cloudfrontDistribution, '--paths', '/*']);
+console.log(`Done.`);
