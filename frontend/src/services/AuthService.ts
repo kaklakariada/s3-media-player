a1123dc82eb881f56e217eb3c8d073cbd867a1fc
kaklakariada <kaklakariada@users.noreply.github.com>
2021-03-14 12:45:18 +0100

    Cleanup

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -1,27 +1,15 @@
 import { Auth } from "aws-amplify";
-import { CognitoUser } from "amazon-cognito-identity-js";
-
 import { ICredentials } from '@aws-amplify/core';
 
 export class AuthService {
 
     async getIdToken(): Promise<string> {
-        const user = await this.currentAuthenticatedUser();
-        const session = user?.getSignInUserSession();
+        const user = await Auth.currentAuthenticatedUser();
+        const session = user.getSignInUserSession();
         if (!session) {
-            throw new Error(`No session found for user ${user?.getUsername()}`);
+            throw new Error(`No session found for user ${user.getUsername()}`);
         }
         return session.getIdToken().getJwtToken();
-    }
-
-    async currentAuthenticatedUser(): Promise<CognitoUser | undefined> {
-        try {
-            const user = Auth.currentAuthenticatedUser();
-            return user;
-        } catch (error) {
-            console.error("Error getting user", error)
-            return undefined; //throw new Error("Error getting user: " + error);
-        }
     }
 
     getCredentials(): Promise<ICredentials> {
