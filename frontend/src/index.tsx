4666ec29243a1b58c8fe269a027fd222fb014ed1
kaklakariada <kaklakariada@users.noreply.github.com>
2023-07-30 14:20:03 +0200

    Revert "Upgrade to react 18"
    This reverts commit 3032dc04ee340c089d9ab9510531497a035947b6.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import Amplify from "aws-amplify";
@@ -9,9 +9,12 @@ import environment from "./environment";
 
 Amplify.configure(environment.amplifyConfig);
 
-const container = document.getElementById('root')
-const root = createRoot(container);
-root.render(<React.StrictMode><App /></React.StrictMode>)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
