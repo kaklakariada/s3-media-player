c0d356f2a7c7c52fc5c11d0feceb939784b7d004
kaklakariada <kaklakariada@users.noreply.github.com>
2023-07-30 15:10:01 +0200

    Fix react render & amplify import

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,16 @@
-import ReactDOM from 'react-dom';
+import { Amplify } from "aws-amplify";
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import environment from "./environment";
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-import Amplify from "aws-amplify";
-import environment from "./environment";
-
 Amplify.configure(environment.amplifyConfig);
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-);
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
