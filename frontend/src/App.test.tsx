60ad24d016b6fc83f1309f09e04a359c33737817
kaklakariada <kaklakariada@users.noreply.github.com>
2024-03-09 16:37:16 +0100

    Upgrade dependencies

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeDefined()
 });
